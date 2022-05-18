87143dbd896a9020807a1a4ddaaaad5c5f49ec5e
James Feltner <jefeltner11@gmail.com>
2022-05-17 22:28:50 -0500

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Calculator
+{
+    public class Calculator : INotifyPropertyChanged
+    {
+
+        private int _display = 0;
+        private int _temporary = 0;
+        private string _operator = "";
+        public int Display
+        {
+            get { return _display; }
+            set
+            {
+                _display = value;
+                OnPropertyChanged("Display");
+            }
+        }
+
+        public int Temporary
+        {
+            get { return _temporary; }
+            set
+            {
+                _temporary = value;
+                OnPropertyChanged("Display");
+            }
+        }
+
+        public string Operator
+        {
+            get { return _operator; }
+            set
+            {
+                _operator = value;
+            }
+        }
+
+        public void Concatenate(int lastDigit)
+        {
+            Display = int.Parse(Display.ToString() + lastDigit.ToString());
+        }
+
+        public void StoreTemp()
+        {
+            Temporary = Display;
+        }
+
+        public void Modulus()
+        {
+            StoreTemp();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calculator = new Calculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = calculator;
+        }
+
+        private void modulus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void clearEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(7);
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(8);
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(9);
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(4);
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(5);
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(6);
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(1);
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(2);
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Concatenate(3);
+        }
+
+
+    }
+}
